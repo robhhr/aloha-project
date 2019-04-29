dc6fc97cf2c48f7d9ffbd57e263d1a7b866b6cc4
Roberto <robhhr@outlook.com>
2019-04-29 16:28:20 -0700

    Mobile & Desktop finished - flickity styling missing

---
+++ JS/flickity-start.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: "center",
+  contain: true,
+  freeScroll: false, 
+  prevNextButtons: false,
+});
